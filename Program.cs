f1486a956860765fc43901389d8c3e03fdf0eadd
BhawtankarAnimesh <159749153+BhawtankarAnimesh@users.noreply.github.com>
2024-02-28 11:14:51 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,11 +7,9 @@
             int[] arr = { 5, 72, 3, 14, 7, 1 };
             int n = arr.Length;
 
-
             for (int i = 0; i < n - 1; i++)
             {
                 int minIndex = i;
-
                 for (int j = i + 1; j < n; j++)
                 {
                     if (arr[j] < arr[minIndex])
@@ -28,7 +26,6 @@
             foreach (int num in arr)
             {
                 Console.Write(num + " ");
-            }
-        }
+            }}
     }
 }
