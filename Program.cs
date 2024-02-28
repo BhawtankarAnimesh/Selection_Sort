506d620186e2483d2bb5bf93fd8f53bff0c801b2
Animesh Bhawtankar <animesh7999@gmail.com>
2024-02-28 11:06:49 -0500

    Selection_Sort

--- Program.cs
+++ Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] arr = { 5, 72, 3, 14, 7, 1 };
+            int n = arr.Length;
+
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+            Console.WriteLine("Sorted array is:");
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
         }
     }
 }
